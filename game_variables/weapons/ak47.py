b50279f99a05bd8318a930de83a859a622ebc831
Lukas <lukas.bader@student.htl-rankweil.at>
2026-05-24 14:06:41 +0200

    ak

---
+++ game_variables/weapons/ak47.py
@@ -0,0 +1,33 @@
+import pygame
+from game_variables.sprites import Sprite
+
+class Ak:
+
+    def __init__(self):
+
+
+        self.gun_anim = Sprite("assets/ak47_1.png",
+                               2, pygame.Rect(0, 0, 64, 48), 0)
+        self.gun_anim.load_spritesheet()
+        self.is_shooting = False
+        self.shoot_frame = 0
+
+    def shoot(self):
+        self.is_shooting = True
+        self.shoot_frame = 0
+
+    def draw(self, screen, x, y, facing_left):
+        if self.is_shooting:
+            frame = self.gun_anim.images[self.shoot_frame]
+            self.shoot_frame += 1
+            if self.shoot_frame >= self.gun_anim.image_count:
+                self.is_shooting = False
+                self.shoot_frame = 0
+        else:
+            frame = self.gun_anim.images[0]
+        if facing_left:
+            frame = pygame.transform.flip(frame, True, False)
+            screen.blit(frame, (x - 20, y + 10))
+
+        else:
+            screen.blit(frame, (x + 10, y + 10))
